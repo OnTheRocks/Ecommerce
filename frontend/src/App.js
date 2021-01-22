252ceee9411625c130b060c1e283ead7c0107151
Nathan Huber <Nathan_Huber@yahoo.com>
2021-01-22 00:54:21 -0600

    Created React App (frontend)

---
+++ frontend/src/App.js
@@ -0,0 +1,152 @@
+
+
+function App() {
+  return (
+    <div class="grid-container">
+    <header class="row">
+      <div>
+        <a class="brand" href="index.html">EStore</a>
+      </div>
+      <div>
+        <a href="cart.html">Cart</a>
+        <a href="signin.html">Sign In</a>
+      </div>
+    </header>
+    <main>
+      <div class="row center">
+        <div class="card">
+          <a href="product.html">
+{/* Image size: 680px by 830px  */}
+            <img class="medium" src="./images/p1.jpg" alt="product" />
+          </a>
+          <div class="card-body">
+            <a href="product.html">
+              <h2>Nike Slim Shirts</h2>
+            </a>
+            <div class="rating">
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star-half-alt"></i></span>
+              <span><i class="far fa-star"></i></span>
+            </div>
+            <div class="price">
+              $77
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="product.html">
+{/* Image size: 680px by 830px  */}
+            <img class="medium" src="./images/p2.jpg" alt="product" />
+          </a>
+          <div class="card-body">
+            <a href="product.html">
+              <h2>Nike Slim Shirts</h2>
+            </a>
+            <div class="rating">
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+            </div>
+            <div class="price">
+              $79
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="product.html">
+{/* Image size: 680px by 830px */}
+            <img class="medium" src="./images/p3.jpg" alt="product" />
+          </a>
+          <div class="card-body">
+            <a href="product.html">
+              <h2>Nike Slim Shirts</h2>
+            </a>
+            <div class="rating">
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star-half-alt"></i></span>
+              <span><i class="far fa-star"></i></span>
+            </div>
+            <div class="price">
+              $79
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="product.html">
+{/* Image size: 680px by 830px */}
+            <img class="medium" src="./images/p4.jpg" alt="product" />
+          </a>
+          <div class="card-body">
+            <a href="product.html">
+              <h2>Nike Slim Shirts</h2>
+            </a>
+            <div class="rating">
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+            </div>
+            <div class="price">
+              $89
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="product.html">
+{/* Image size: 680px by 830px */}
+            <img class="medium" src="./images/p5.jpg" alt="product" />
+          </a>
+          <div class="card-body">
+            <a href="product.html">
+              <h2>Nike Slim Shirts</h2>
+            </a>
+            <div class="rating">
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+              <span><i class="fas fa-star"></i></span>
+            </div>
+            <div class="price">
+              $89
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="product.html">
+            {/* Image size: 680px by 830px */}
+            <img class="medium" src="./images/p6.jpg" alt="product" />
+          </a>
+          <div class="card-body">
+            <a href="product.html">
+              <h2>Nike Slim Shirts</h2>
+            </a>
+            <div class="rating">
+              <span><i class="fa fa-star"></i></span>
+              <span><i class="fa fa-star"></i></span>
+              <span><i class="fa fa-star"></i></span>
+              <span><i class="fas fa-star-half-alt"></i></span>
+              <span><i class="far fa-star"></i></span>
+            </div>
+            <div class="price">
+              $99
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="row center" >
+      All Rights Reserved
+    </footer>
+  </div>
+  );
+}
+
+export default App;
