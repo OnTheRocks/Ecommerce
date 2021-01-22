8128e45aa8df62620e2bec38df5f16dc1d98761a
Nathan Huber <Nathan_Huber@yahoo.com>
2021-01-22 01:03:34 -0600

    Work on Product section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@
 
 function App() {
   return (
-    <div class="grid-container">
-    <header class="row">
+    <div className="grid-container">
+    <header className="row">
       <div>
-        <a class="brand" href="index.html">EStore</a>
+        <a className="brand" href="index.html">EStore</a>
       </div>
       <div>
         <a href="cart.html">Cart</a>
@@ -13,136 +13,136 @@ function App() {
       </div>
     </header>
     <main>
-      <div class="row center">
-        <div class="card">
+      <div className="row center">
+        <div className="card">
           <a href="product.html">
 {/* Image size: 680px by 830px  */}
-            <img class="medium" src="./images/p1.jpg" alt="product" />
+            <img className="medium" src="./images/p1.jpg" alt="product" />
           </a>
-          <div class="card-body">
+          <div className="card-body">
             <a href="product.html">
               <h2>Nike Slim Shirts</h2>
             </a>
-            <div class="rating">
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star-half-alt"></i></span>
-              <span><i class="far fa-star"></i></span>
+            <div className="rating">
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star-half-alt"></i></span>
+              <span><i className="far fa-star"></i></span>
             </div>
-            <div class="price">
+            <div className="price">
               $77
             </div>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <a href="product.html">
 {/* Image size: 680px by 830px  */}
-            <img class="medium" src="./images/p2.jpg" alt="product" />
+            <img className="medium" src="./images/p2.jpg" alt="product" />
           </a>
-          <div class="card-body">
+          <div className="card-body">
             <a href="product.html">
               <h2>Nike Slim Shirts</h2>
             </a>
-            <div class="rating">
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
+            <div className="rating">
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
             </div>
-            <div class="price">
+            <div className="price">
               $79
             </div>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <a href="product.html">
 {/* Image size: 680px by 830px */}
-            <img class="medium" src="./images/p3.jpg" alt="product" />
+            <img className="medium" src="./images/p3.jpg" alt="product" />
           </a>
-          <div class="card-body">
+          <div className="card-body">
             <a href="product.html">
               <h2>Nike Slim Shirts</h2>
             </a>
-            <div class="rating">
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star-half-alt"></i></span>
-              <span><i class="far fa-star"></i></span>
+            <div className="rating">
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star-half-alt"></i></span>
+              <span><i className="far fa-star"></i></span>
             </div>
-            <div class="price">
+            <div className="price">
               $79
             </div>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <a href="product.html">
 {/* Image size: 680px by 830px */}
-            <img class="medium" src="./images/p4.jpg" alt="product" />
+            <img className="medium" src="./images/p4.jpg" alt="product" />
           </a>
-          <div class="card-body">
+          <div className="card-body">
             <a href="product.html">
               <h2>Nike Slim Shirts</h2>
             </a>
-            <div class="rating">
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
+            <div className="rating">
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
             </div>
-            <div class="price">
+            <div className="price">
               $89
             </div>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <a href="product.html">
 {/* Image size: 680px by 830px */}
-            <img class="medium" src="./images/p5.jpg" alt="product" />
+            <img className="medium" src="./images/p5.jpg" alt="product" />
           </a>
-          <div class="card-body">
+          <div className="card-body">
             <a href="product.html">
               <h2>Nike Slim Shirts</h2>
             </a>
-            <div class="rating">
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
+            <div className="rating">
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
+              <span><i className="fas fa-star"></i></span>
             </div>
-            <div class="price">
+            <div className="price">
               $89
             </div>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <a href="product.html">
             {/* Image size: 680px by 830px */}
-            <img class="medium" src="./images/p6.jpg" alt="product" />
+            <img className="medium" src="./images/p6.jpg" alt="product" />
           </a>
-          <div class="card-body">
+          <div className="card-body">
             <a href="product.html">
               <h2>Nike Slim Shirts</h2>
             </a>
-            <div class="rating">
-              <span><i class="fa fa-star"></i></span>
-              <span><i class="fa fa-star"></i></span>
-              <span><i class="fa fa-star"></i></span>
-              <span><i class="fas fa-star-half-alt"></i></span>
-              <span><i class="far fa-star"></i></span>
+            <div className="rating">
+              <span><i className="fa fa-star"></i></span>
+              <span><i className="fa fa-star"></i></span>
+              <span><i className="fa fa-star"></i></span>
+              <span><i className="fas fa-star-half-alt"></i></span>
+              <span><i className="far fa-star"></i></span>
             </div>
-            <div class="price">
+            <div className="price">
               $99
             </div>
           </div>
         </div>
       </div>
     </main>
-    <footer class="row center" >
+    <footer className="row center" >
       All Rights Reserved
     </footer>
   </div>
