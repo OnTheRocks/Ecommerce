d0852286a28137443329787e888c07d2ed25a4fe
Nathan Huber <Nathan_Huber@yahoo.com>
2021-01-23 01:54:56 -0600

    Updated product section with array, replacing static data.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,145 +1,44 @@
-
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
     <div className="grid-container">
     <header className="row">
       <div>
-        <a className="brand" href="index.html">EStore</a>
+        <a className="brand" href="/">EStore</a>
       </div>
       <div>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
       </div>
     </header>
     <main>
       <div className="row center">
-        <div className="card">
-          <a href="product.html">
-{/* Image size: 680px by 830px  */}
-            <img className="medium" src="./images/p1.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Nike Slim Shirts</h2>
+        {data.products.map(product => (
+            <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+  {/* Image size: 680px by 830px  */}
+              <img className="medium" src={product.image} alt={product.name} />
             </a>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-alt"></i></span>
-              <span><i className="far fa-star"></i></span>
+            <div className="card-body">
+              <a href={`/product/${product._id}`}>
+                <h2>{product.name}</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fas fa-star"></i></span>
+                <span><i className="fas fa-star"></i></span>
+                <span><i className="fas fa-star"></i></span>
+                <span><i className="fas fa-star-half-alt"></i></span>
+                <span><i className="far fa-star"></i></span>
+              </div>
+              <div className="price">
+                ${product.price}
+              </div>
             </div>
-            <div className="price">
-              $77
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-{/* Image size: 680px by 830px  */}
-            <img className="medium" src="./images/p2.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Nike Slim Shirts</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-            </div>
-            <div className="price">
-              $79
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-{/* Image size: 680px by 830px */}
-            <img className="medium" src="./images/p3.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Nike Slim Shirts</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-alt"></i></span>
-              <span><i className="far fa-star"></i></span>
-            </div>
-            <div className="price">
-              $79
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-{/* Image size: 680px by 830px */}
-            <img className="medium" src="./images/p4.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Nike Slim Shirts</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-            </div>
-            <div className="price">
-              $89
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-{/* Image size: 680px by 830px */}
-            <img className="medium" src="./images/p5.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Nike Slim Shirts</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-            </div>
-            <div className="price">
-              $89
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-            {/* Image size: 680px by 830px */}
-            <img className="medium" src="./images/p6.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Nike Slim Shirts</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fas fa-star-half-alt"></i></span>
-              <span><i className="far fa-star"></i></span>
-            </div>
-            <div className="price">
-              $99
-            </div>
-          </div>
-        </div>
+          </div> 
+          ))}
+       
       </div>
     </main>
     <footer className="row center" >
