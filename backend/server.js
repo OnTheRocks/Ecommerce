f2594faf73292a8e804bfd02d16503c8b1b7bf1f
Nathan Huber <Nathan_Huber@yahoo.com>
2021-01-27 23:27:56 -0600

    Added nodemon and route for api/products.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import data from './data.js';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send("Server is up");
 });
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+})
 
 const port = process.env.PORT || 5454;
 
