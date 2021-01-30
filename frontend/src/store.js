c886a5a6ca649aa07ee38c44b5fd7bfe579f293b
Nathan Huber <Nathan_Huber@yahoo.com>
2021-01-30 01:27:22 -0600

    connected store to redux devtools.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,16 @@
-import {createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
 import data from "./data";
 
 const initialState = {};
 const reducer = (state, action) => {
   return {products: data.products};
 };
-
-const store = createStore(reducer, initialState);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+  );
 
 export default store;
