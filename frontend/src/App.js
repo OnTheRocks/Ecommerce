04bc963af86aa75c9e82d327788b94626d0031ad
Nathan Huber <Nathan_Huber@yahoo.com>
2021-02-07 01:05:36 -0600

    Added cart screen, add to cart function and Qty function.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import CartScreen from './Screens/CartScreen';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 
@@ -17,6 +18,7 @@ function App() {
       </div>
     </header>
     <main>
+      <Route path="/cart/:id?" component={CartScreen}></Route>
       <Route path="/product/:id" component={ProductScreen}></Route>
       <Route path="/" component={HomeScreen} exact></Route>
 
