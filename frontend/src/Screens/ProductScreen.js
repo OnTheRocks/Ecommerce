a0c3011f0b5d6420e480f3298b4dd5828452bd12
Nathan Huber <Nathan_Huber@yahoo.com>
2021-02-08 23:44:02 -0600

    Work in cartScreen.js

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -79,8 +79,7 @@ export default function ProductScreen(props) {
                     <div>Qty</div>
                     <div>
                       <select value={qty} onChange={e => setQty(e.target.value)}>
-                        {
-                          [...Array(product.countInStock).keys()].map( x => (
+                        {[...Array(product.countInStock).keys()].map( x => (
                             <option key={x + 1} value={x + 1}>
                               {x + 1}
                             </option>
