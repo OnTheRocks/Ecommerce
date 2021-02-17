d6839dd6d1548fbba1ba776c90dc06384c358d24
Nathan Huber <Nathan_Huber@yahoo.com>
2021-02-17 02:08:26 -0600

    Implemented SignIn action.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from "../constants/userConstants"
+import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "../constants/userConstants"
 
 export const signin = (email, password)  => async(dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -14,4 +14,9 @@ export const signin = (email, password)  => async(dispatch) => {
     ? error.response.data.message
     : error.message, })
   }
+};
+export const signout = () => (dispatch) =>{
+  localStorage.removeItem('userInfo');
+  localStorage.removeItem('cartItems');
+  dispatch({ type: USER_SIGNOUT });
 };
