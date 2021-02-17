c577721886fb8139e7334e5f11a1a627868e2cee
Nathan Huber <Nathan_Huber@yahoo.com>
2021-02-16 23:02:10 -0700

    Created SignIN route.

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -11,6 +11,20 @@ userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
   res.send({ createdUsers });
 }));
 
-userRouter.post('/signin', expressAsyncHandler(async (req, res)))
-
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if(user) {
+    if(bcrypt.comareSync(req.body.password, user.password)) {
+      res.send({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      });
+      return;
+    }
+  }
+  res.status(401).send({ message: 'Invalid email or password' });
+}));
 export default userRouter;
