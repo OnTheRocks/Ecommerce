ce7d284e4c3375abecec90118affad58d381dbfc
Nathan Huber <Nathan_Huber@yahoo.com>
2021-02-22 00:18:32 -0600

    Created POST API for orders.

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
 
 export const generateToken = (user) => {
   return jwt.sign(
@@ -13,4 +13,22 @@ export const generateToken = (user) => {
       expiresIn: '30d',
     }
   );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(authorization){
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+    if (err) {
+      req.status(401).send({ message: 'Invalid Token' });
+    } else {
+      req.user = decode;
+      next();
+    }
+  }
+);
+  } else {
+    req.status(401).send({ message: 'No Token' });
+  }
 };
