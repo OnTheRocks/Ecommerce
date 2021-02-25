44db7ad5eb6f61aad8fc633eca67abb52511c849
Nathan Huber <Nathan_Huber@yahoo.com>
2021-02-24 23:57:19 -0600

    Added PayPal SDK.

--- frontend/src/Screens/OrderScreen.js
+++ frontend/src/Screens/OrderScreen.js
@@ -1,3 +1,4 @@
+import Axios from 'axios';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
@@ -7,12 +8,35 @@ import MessageBox from '../components/MessageBox';
 
 export default function OrderScreen(props) {
   const orderId = props.match.params.id;
+  const [sdkReady, setSdkReady] = useState(false);
   const orderDetails = useSelector((state) => state.orderDetails);
   const { order, loading, error } = orderDetails;
   const dispatch = useDispatch();
   useEffect(() => {
+    const addPayPalScript = async () => {
+      const { data } = await Axios.get('/api/config/paypal');
+      const script = document.createElement('script');
+      script.type="text/javascript";
+      script.src=`https://www.paypal.com/sdk/js?client-id=${data}`;
+      script.async = true;
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      document.body.appendChild(script);
+    };
+    if(!order._id) {
+      dispatch(detailsOrder(orderId));
+    } else {
+      if(!order.isPaid) {
+        if(!window.paypal) {
+          addPayPalScript();
+        } else {
+          sdkReady(true);
+        }
+      }
+    }
     dispatch(detailsOrder(orderId));
-  }, [dispatch, orderId]);
+  }, [dispatch, order, orderId, sdkReady]);
   return loading ? (
     <LoadingBox></LoadingBox>
   ) : error ? (
