78a81e024ccd17952810bce20499bb74f706cd86
Nathan Huber <Nathan_Huber@yahoo.com>
2021-03-13 00:31:49 -0600

    Order Delete function complete.

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -14,7 +14,10 @@ import { ORDER_CREATE_FAIL,
          ORDER_LIST_MINE_SUCCESS,
          ORDER_LIST_SUCCESS,
          ORDER_LIST_FAIL,
-         ORDER_LIST_REQUEST} from "../constants/orderConstants"
+         ORDER_LIST_REQUEST,
+         ORDER_DELETE_REQUEST,
+         ORDER_DELETE_SUCCESS,
+         ORDER_DELETE_FAIL} from "../constants/orderConstants"
 
 export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({type: ORDER_CREATE_REQUEST, payload: order});
@@ -105,5 +108,22 @@ export const listOrders = () => async (dispatch, getState) => {
       ? error.response.data.message
       : error.message;
     dispatch({ type: ORDER_LIST_FAIL, payload: message});
+  }
+}
+
+export const deleteOrder = (orderId) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_DELETE_REQUEST, payload: orderId });
+  const { userSignin:{ userInfo }} = getState();
+  try {
+    const{ data } = await Axios.delete(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}`, },
+    });
+    dispatch({ type: ORDER_DELETE_SUCCESS, payload: data });
+  } catch(error) {
+    const message = 
+      error.response && error.response.data.message
+      ? error.response.data.message
+      : error.message;
+    dispatch({ type: ORDER_DELETE_FAIL, payload: message});
   }
 }
