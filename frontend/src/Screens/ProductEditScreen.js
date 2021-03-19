30bb0da801504838702255037aea55f80235e584
Nathan Huber <Nathan_Huber@yahoo.com>
2021-03-19 16:30:46 -0500

    edited productEditScreen.

--- frontend/src/Screens/ProductEditScreen.js
+++ frontend/src/Screens/ProductEditScreen.js
@@ -57,7 +57,7 @@ export default function ProductEditScreen(props) {
     bodyFormData.append('image', file);
     setLoadingUpload(true);
     try {
-      const {data} = await Axios.post('/api/uploads', bodyFormData, {
+      const {data} = await Axios.post('/api/images', bodyFormData, {
         headers: { 'Content-Type': 'multipart/form-data', Authorization: `Bearer ${userInfo.token}`,},
       });
       setImage(data);
