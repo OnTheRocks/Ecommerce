b75f09996d553c7de835eb4f49496cdf0d5e1ec3
Nathan Huber <Nathan_Huber@yahoo.com>
2021-03-30 23:51:34 -0500

    Added code to reduce qty number when item purchased.  Doesn't work yet though.

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -62,6 +62,7 @@ orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) => {
     order.paidAt = Date.now();
     order.paymentResult = { id: req.body.id, status: req.body.status, update_time: req.body.update_time, email_address: req.body.email_address };
     const updateOrder = await order.save();
+ 
     res.send({ message: 'Order Paid', order: updateOrder });
   } else {
     res.status(404).send({ message: 'Order Not Found' });
